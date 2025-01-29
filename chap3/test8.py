ec903e5c9c20a19bbf219616de9201ff227b3aa2
AdrienMertens <mertensadrien23@hotmail.be>
2025-01-29 18:48:46 +0100

    exercice 3.8 ok

---
+++ chap3/test8.py
@@ -0,0 +1,13 @@
+"""
+ Écrivez un algorithme qui réponde True si un nombre donné par l’utilisateur est pair et
+ divisible par 5.
+
+__author__ = "Mertens Adrien"
+__version__ = "1.0"
+"""
+
+#assign and input
+number = int(input("Veuillez un nombre entier : "))
+
+# check is number is impair or divisible by 5
+print(f"est ce que le nombre ({number}) est pair et divisible par 5 ? {number % 5 == 0 and number % 2 == 0}")
