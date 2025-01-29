bdef1ae5c2e9c2c3966898efc99c3a0eb0badb26
AdrienMertens <mertensadrien23@hotmail.be>
2025-01-29 19:02:55 +0100

    exercice 3.10 ok

---
+++ chap3/test10.py
@@ -0,0 +1,12 @@
+"""
+ Écrivez un algorithme qui réponde True si un mot donné par l’utilisateur est après le mot
+ combinatoire et avant le mot logique dans le dictionnaire.
+
+__author__ = "Mertens Adrien"
+__version__ = "1.0"
+"""
+#assign and input
+answer_user = input("Veuillez entrez un mots : ")
+
+# check if answer_user is before combinatoire
+print(f"le mot ({answer_user}) est après le mot (combinatoire) et avant le mot (logique): {answer_user > "combinatoire" and answer_user < "logique" }")
