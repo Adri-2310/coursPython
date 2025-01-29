fea3f4c48e2f81e106fd79a1b694c0494425b388
AdrienMertens <mertensadrien23@hotmail.be>
2025-01-29 19:30:13 +0100

    exo 4.1 ok

---
+++ chap4/chap4.py
@@ -0,0 +1,20 @@
+def positifOrNegatif():
+    """
+     Écrivez un algorithme qui demande un nombre à l’utilisateur, puis qui l’informe si ce
+     nombre est positif ou négatif. On considère que 0 est positif.
+
+    __author__ = "Mertens Adrien"
+    __version__ = "1.0"
+    """
+
+    # assign and input
+    number = int(input("Veuillez entrez un nombre entier: "))
+
+    # check if number is positif or negative
+    if number >= 0:
+        print(f"{number} est positif.")
+    else:
+        print(f"{number} est negatif.")
+
+if __name__ == '__main__':
+    positifOrNegatif()
