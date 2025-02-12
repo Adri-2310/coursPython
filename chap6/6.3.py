8e24669917ee405e3c44ca79cfef81b5b90bfdd5
AdrienMertens <mertensadrien23@hotmail.be>
2025-02-12 19:05:40 +0100

    exo 6.3 ok

---
+++ chap6/6.3.py
@@ -0,0 +1,8 @@
+"""
+__author__= "Adrien Mertens"
+__version__ = "1.0"
+"""
+number_user = int(input("Veuillez entré un nombre: "))
+
+for i in range(1,11):
+    print(f"Le nombre suivant est : {number_user+i}")
